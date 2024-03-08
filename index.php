cf03ec3dc7a6fad7ba5c70c03b49664f3d0a4fd3
Lenício Jr <lenicio.junior@hotmail.com>
2024-03-08 17:12:04 -0300

    Finalização da revisão

--- index.php
+++ index.php
@@ -1,23 +1,57 @@
 <?php
+
+/*
+
+  Atividade avaliativa:
+
+  - Implementar a lógica onde apareça na lista o nome da pessoa mais velha e mais nova
+    que está no banco de dados.
+
+  Pontuação:
+    10% das atividades em sala.
+
+*/
+
 require "./config.php";
 
 $sql = "SELECT * FROM pessoas";
 $sql = $pdo->prepare($sql);
 $sql->execute();
-
 $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 $contMaiorIdade = 0;
 $contMenorIdade = 0;
-foreach ($dados as $linha) {
+
+$maiorIdade = 0;
+$maiorIdadeNome = "";
+
+$menorIdade = PHP_INT_MAX;
+$menorIdadeNome = "";
+
+foreach ($dados as $key => $linha) {
+  $idade = $linha['idade'];
+
+  // Esta condicional armazena a maior idade da lista
+  if ($idade > $maiorIdade) {
+    $maiorIdade = $idade;
+    $maiorIdadeNome = $linha['nome'];
+  }
+
+  // Esta condicional armazena a menor idade da lista
+  if ($idade < $menorIdade) {
+    $menorIdade = $idade;
+    $menorIdadeNome = $linha['nome'];
+  }
+
 
   if($linha['idade'] >= 18) {
    $contMaiorIdade++; 
   } else {
     $contMenorIdade++;
   }
-
 }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -37,10 +71,11 @@ foreach ($dados as $linha) {
     <input type="submit" value="Enviar">
   </form>
 
-
   <ul>
     <li><?=$contMaiorIdade?> pessoas são maiores de idade.</li>
     <li><?=$contMenorIdade?> pessoas são menores de idade.</li>
+    <li><?=$maiorIdadeNome?> é a pessoa mais velha, com <?=$maiorIdade?> anos.</li>
+    <li><?=$menorIdadeNome?> é a pessoa mais nova, com <?=$menorIdade?> anos.</li>
   </ul>
 </body>
 
